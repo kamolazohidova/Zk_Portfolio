784ee9f5e2374c6bfcd15cafc6c478c4d51b945a
Kzohidova <kamolazohidova04@gmail.com>
2022-06-18 12:32:11 +0500

    Add main and list section

--- js/main.js
+++ js/main.js
@@ -1,13 +1,11 @@
 const elbody = document.querySelector('.body');
 const lightButton = document.querySelector('.site-header-top__light');
 const darkButton = document.querySelector('.site-header-top__dark');
-
-
-lightButton.addEventListener('click', function(){
+lightButton.addEventListener('click', function () {
     elbody.style.backgroundColor = '#fff'
 });
 
-darkButton.addEventListener('click', function(){
+darkButton.addEventListener('click', function () {
     elbody.style.backgroundColor = '#000'
 });
 
